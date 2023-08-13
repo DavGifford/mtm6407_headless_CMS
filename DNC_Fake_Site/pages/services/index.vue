dc20c655bbd908b05b6bb142d9752be2f066638c
Nas Samoyloff <samo0018@algonquinlive.com>
2023-08-13 15:52:59 -0400

    added services

--- DNC_Fake_Site/pages/services/index.vue
+++ DNC_Fake_Site/pages/services/index.vue
@@ -1,6 +1,12 @@
 <template>
       <div>
           <h2>Our Services Page</h2>
+            <div v-for="block in story.body" :key="block._uid">
+                <Alert v-if="block.component === 'Alert'" :blok="block" />
+                <Card v-if="block.component === 'card'" :blok="block" />
+                <Teaser v-if="block.component === 'teaser'" :blok="block" />
+                <Grid v-if="block.component === 'grid'" :blok="block" />
+	        </div>
       </div>
   </template>
   
