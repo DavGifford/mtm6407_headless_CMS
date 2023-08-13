d1a7558dc9b5e8be7d33fe1851cddd4f0f7dff73
Charlie Guenther <guen0032@algonquinlive.com>
2023-08-13 16:40:07 -0400

    updates

--- DNC_Fake_Site/storyblok/Grid.vue
+++ DNC_Fake_Site/storyblok/Grid.vue
@@ -1,15 +1,17 @@
 <template>
+  <div
+    v-editable="blok"
+    class="container mx-auto grid gap-12 my-12 place-items-center"
+  >
     <div
-      v-editable="blok"
-      class="container mx-auto grid gap-12 my-12 place-items-center"
+      v-for="blok in blok.columns"
+      :key="blok._uid"
+      class="w-full md:w-full lg:w-1/2"
     >
-      <StoryblokComponent
-        v-for="blok in blok.columns"
-        :key="blok._uid"
-        :blok="blok"
-      />
+      <StoryblokComponent :blok="blok" />
     </div>
-  </template>
+  </div>
+</template>
    
   <script setup>
   defineProps({ blok: Object })
