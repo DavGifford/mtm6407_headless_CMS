a3a1c1daefba09c60fce4d86c7e03124c53bdb95
Charlie Guenther <guen0032@algonquinlive.com>
2023-08-13 15:06:11 -0400

    rounded image

--- DNC_Fake_Site/storyblok/Feature.vue
+++ DNC_Fake_Site/storyblok/Feature.vue
@@ -3,7 +3,7 @@
       v-editable="blok"
       class="w-full p-12 bg-[#f7f6fd] rounded-xl"
     >
-    <div class="flex justify-center"><img v-if="blok.image" :src="blok.image.filename" :alt="blok.image.alt" /></div>
+    <div class="flex justify-center"><img class="rounded-full" v-if="blok.image" :src="blok.image.filename" :alt="blok.image.alt" /></div>
       <h3 class="text-2xl text-[#1d243d] font-bold my-2">
         {{ blok.name }}
       </h3>
