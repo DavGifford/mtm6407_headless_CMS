5e450589207b8da7857084be87cc201ca6cb69f4
Charlie Guenther <guen0032@algonquinlive.com>
2023-08-13 13:23:38 -0400

    attempted fixes

--- DNC_Fake_Site/pages/index.vue
+++ DNC_Fake_Site/pages/index.vue
@@ -1,4 +1,12 @@
+<script setup>
+const story = await useAsyncStoryblok('home', { version: 'draft' })
+</script>
+ 
 <template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
+
+<!-- <template>
     <div>
         <h2>Home</h2>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque nesciunt enim obcaecati, necessitatibus facilis est quibusdam modi! Exercitationem, quaerat cumque.</p>
@@ -19,4 +27,4 @@ h2 {
 p {
     margin: 20px 0;
 }
-</style>
+</style> -->
--- DNC_Fake_Site/tailwind.config.js
+++ DNC_Fake_Site/tailwind.config.js
@@ -1,9 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		'storyblok/**/*.{vue,js}',
+		'components/**/*.{vue,js}',
+		'pages/**/*.vue',
+	],
+};
 
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
